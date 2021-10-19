f45604143a487a8a04682cad104aaf281cff7117
Liupaperbox <hash_548@163.com>
2021-10-19 21:05:25 +0800

    change name

--- src/database/db.go
+++ src/database/db.go
@@ -1,14 +1,15 @@
 package database
 
 import (
-	"github.com/Rorical/NearDB/src/utils"
-	"github.com/syndtr/goleveldb/leveldb"
 	"sort"
 	"strings"
+
+	"github.com/Rorical/NearDB/src/utils"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 type NearDBDatabase struct {
-	index *utils.LshForest
+	index    *utils.LshForest
 	database *leveldb.DB
 	datasize int
 }
@@ -20,18 +21,18 @@ func NewDatabase() (*NearDBDatabase, error) {
 		return nil, err
 	}
 	return &NearDBDatabase{
-		index: utils.NewLshForest(size, 6, 3, 3),
+		index:    utils.NewLshForest(size, 6, 3, 3),
 		datasize: size,
 		database: db,
 	}, nil
 }
 
-func (db *NearDBDatabase) Add(id string, taglist []string) error {
-	err := db.database.Put(utils.StringIn(id), utils.StringIn(strings.Join(taglist, ",")), nil)
+func (db *NearDBDatabase) Add(id string, set []string) error {
+	err := db.database.Put(utils.StringIn(id), utils.StringIn(strings.Join(set, ",")), nil)
 	if err != nil {
 		return err
 	}
-	point := utils.CompHash(taglist, db.datasize)
+	point := utils.CompHash(set, db.datasize)
 	db.index.Insert(point, id)
 	return nil
 }
@@ -40,16 +41,16 @@ func (db *NearDBDatabase) Refresh() {
 	iter := db.database.NewIterator(nil, nil)
 	for iter.Next() {
 		id := utils.StringOut(iter.Key())
-		taglist := strings.Split(utils.StringOut(iter.Value()), ",")
-		db.Add(id, taglist)
+		set := strings.Split(utils.StringOut(iter.Value()), ",")
+		db.Add(id, set)
 	}
 	iter.Release()
 }
 
-func (db *NearDBDatabase) Query(taglist []string, k int) (utils.ItemList, error) {
-	point := utils.CompHash(taglist, db.datasize)
+func (db *NearDBDatabase) Query(set []string, k int) (utils.ItemList, error) {
+	point := utils.CompHash(set, db.datasize)
 	unsortedresult := db.index.Query(point, k)
-	originalset := utils.GenSet(taglist)
+	originalset := utils.GenSet(set)
 	itemlist := utils.NewItemList(len(unsortedresult))
 	for _, id := range unsortedresult {
 		data, _ := db.database.Get(utils.StringIn(id), nil)
